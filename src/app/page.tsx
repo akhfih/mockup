8b722fb2feb58819ccb380f4c217ef8fe56bf8bd
akhfih <akhfih.dev@gmail.com>
2025-06-22 17:17:01 +0700

    layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ const HomePage = () => {
   return (
     <div>
       <h2 className="text-lg">Home Page</h2>
-      <Link href={ticketsPath} className="text-blue-500 hover:underline">
+      <Link href={ticketsPath()} className="text-blue-500 hover:underline">
         Go to Tickets
       </Link>
     </div>)
