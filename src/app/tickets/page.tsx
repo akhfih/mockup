ee581d7e02addc464b2a690d2428f8babcf808e1
akhfih <akhfih.dev@gmail.com>
2025-06-23 19:27:26 +0700

    feature folder

--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -1,22 +1,11 @@
 
-import { LucideCircleCheck, LucideFileText, LucidePen } from "lucide-react";
-import Link from "next/link";
-import { Heading } from "@/components/heading";
-import {
-    Card,
-    CardContent,
-    CardFooter,
-    CardHeader,
-    CardTitle,
-} from "@/components/ui/card"
-import { initialTickets } from "@/data";
-import { ticketPath } from "@/paths";
 
-const TICKET_ICONS = {
-    OPEN: <LucideCircleCheck />,
-    DONE: <LucidePen />,
-    IN_PROGRESS: <LucideFileText />,
-}
+import { Heading } from "@/components/heading";
+import { initialTickets } from "@/data";
+import { TicketItem } from "@/features/ticket/components/ticket-item";
+
+
+
 const TicketsPage = () => {
     return (
         <div className="flex-1 flex flex-col gap-y-8">
@@ -24,21 +13,7 @@ const TicketsPage = () => {
             <Heading title="Tickets" description="All your tickets at one place" />
             <div className="flex-1 flex flex-col items-center gap-y-4 animate-fade-in-from-top">
                 {initialTickets.map((ticket) => (
-                    <Card key={ticket.id} className="w-full max-w-[420px]">
-                        <CardHeader>
-                            <CardTitle className="flex gap-x-2">
-                                <span className="">{TICKET_ICONS[ticket.status]}</span>
-                                <span className="truncate">{ticket.title}</span>
-                            </CardTitle>
-
-                        </CardHeader>
-                        <CardContent>
-                            <span className="line-clamp-3 whitespace-break-spaces">{ticket.content}</span>
-                        </CardContent>
-                        <CardFooter>
-                            <Link href={ticketPath(ticket.id)} className="text-sm underline">View </Link>
-                        </CardFooter>
-                    </Card>
+                   <TicketItem key={ticket.id} ticket={ticket}/>
 
                 ))}
             </div>
---
+++ src/features/ticket/constants.tsx
@@ -0,0 +1,7 @@
+import { LucideCircleCheck, LucideFileText, LucidePen } from "lucide-react";
+
+export const TICKET_ICONS = {
+    OPEN: <LucideCircleCheck />,
+    DONE: <LucidePen />,
+    IN_PROGRESS: <LucideFileText />,
+}
