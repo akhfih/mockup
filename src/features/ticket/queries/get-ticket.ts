9f3aaf0306f7e29708f8153dc7e62c0ed33848be
akhfih <akhfih.dev@gmail.com>
2025-06-24 16:56:10 +0700

    error route

--- src/features/ticket/queries/get-ticket.ts
+++ src/features/ticket/queries/get-ticket.ts
@@ -2,7 +2,7 @@ import { initialTickets } from "@/data";
 import { Ticket } from "../types";
 
 export const getTicket = async (ticketId: string): Promise<Ticket | null> => {
-    await new Promise((resolve) => setTimeout(resolve, 1000)); // Simulate network delay
+    await new Promise((resolve) => setTimeout(resolve, 2000)); // Simulate network delay
     const maybeTicket = initialTickets.find((ticket) => ticket.id === ticketId);
     return new Promise((resolve) => {
         resolve(maybeTicket || null);
--- src/features/ticket/queries/get-tickets.ts
+++ src/features/ticket/queries/get-tickets.ts
@@ -3,7 +3,10 @@ import { Ticket } from '../types';
 
 export const getTickets = async (): Promise<Ticket[]> => {
 
-    await new Promise((resolve) => setTimeout(resolve, 1000)); // Simulate network delay
+    await new Promise((resolve) => setTimeout(resolve, 2000)); // Simulate network delay
+
+    // throw new Error('Failed to fetch tickets'); // Placeholder for future implementation
+
     return new Promise((resolve) => {
         resolve(initialTickets);
     });
