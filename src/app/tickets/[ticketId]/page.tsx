bda4b8d5fdb2a5b88d01c2642f2bc56c5a909e39
akhfih <akhfih.dev@gmail.com>
2025-06-24 17:47:44 +0700

    not found route

--- src/app/tickets/[ticketId]/page.tsx
+++ src/app/tickets/[ticketId]/page.tsx
@@ -1,37 +1,16 @@
-import Link from "next/link";
+
+import { notFound } from "next/navigation";
 import React from "react";
-import { Placeholder } from "@/components/placeholder";
-import { Button } from "@/components/ui/button";
 import { TicketItem } from "@/features/ticket/components/ticket-item";
 import { getTicket } from "@/features/ticket/queries/get-ticket";
-import { ticketsPath } from "@/paths";
 
-
-// type TicketsPageProps = {
-//     params: Promise<{
-//         ticketId: string;
-//     }>;
-// };
 
 const TicketsPage = async (props: { params: Promise<{ ticketId: string }> }) => {
     const params = await props.params;
     const ticket = await getTicket(params.ticketId);
 
     if (!ticket) {
-        return (
-            <div className="flex-1 flex">
-                <Placeholder
-                    label="Ticket not found"
-                    button={
-                        <Button asChild variant="outline">
-                            <Link href={ticketsPath()}>Go to tickets</Link>
-                        </Button>
-                    } />
-                <Placeholder
-                    label="Ticket not found"
-                />
-            </div>
-        )
+        notFound();
 
     }
     return (
